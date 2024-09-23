9599c9f374834b7c743b8fba7d3bdc4272a40a53
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-23 03:02:54 +0300

    added quest class,deleted database and current model classes

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,33 +1,21 @@
-from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy import Column, Integer, String,Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base
 
-class BucketListItem(Base):
-    __tablename__ = 'bucket_list_items'
+class Quest(Base):
+    __tablename__ = 'quests'
 
     id = Column(Integer, primary_key=True)
-    description = Column(String, nullable=False)
-    completed = Column(Integer, default=0)
-    destination_id = Column(Integer, ForeignKey('destinations.id'))
-
-    destination = relationship("Destination", back_populates="bucket_list_items")
-
-    def __repr__(self):
-        return f"<BucketListItem(description={self.description}, completed={self.completed})>"
+    title = Column(String, nullable=False)
+    description = Column(String)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    completed_at = Column(DateTime)
+    deadline = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('users.id'))
     
-    def show_bucket_list(session):
-        items = session.query(BucketListItem).all()
-        for item in items:
-            print(f"Item: {item.description}, Destination: {item.description.name}, Completed: {item.completed}")
+
+    user = relationship("User", back_populates="quests")
+
     
-class Destination(Base):
-    __tablename__ = 'destinations'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    location = Column(String, nullable=False)
-
-    bucket_list_items = relationship("BucketListItem", back_populates="destination")
-
-    def __repr__(self):
-        return f"<Destination(name={self.name}, location={self.location})>"
